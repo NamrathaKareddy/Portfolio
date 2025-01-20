a42ab4d7fe1b3911bc3ca26b8c4f7c6d5fe68f1c
Namartha Kareddy <namrathareddykareddy@gmail.com>
2025-01-20 16:22:29 -0500

    git pages dependencies

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "/Users/namratha/Desktop/Portfolio-master/src/Assets/Namratha_Resume.pdf";
+import pdf from "../../Assets/Namratha_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
